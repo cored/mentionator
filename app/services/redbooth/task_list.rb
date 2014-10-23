21d37138028b2c9447b4c5116c2675923464bf88
Rafael George <george.rafael@gmail.com>
2014-10-22 20:39:20 -0400

    Add a proper value object for task lists

--- app/services/redbooth/task_list.rb
+++ app/services/redbooth/task_list.rb
@@ -1,9 +1,29 @@
 module Redbooth 
-  module TaskList 
-    extend self 
-    
-    def call(client)
-      JSON.parse(client.http_client.get("/api/3/task_lists/?access_token=#{client.token}").body)
+  class TaskList 
+    def self.retrieve(client) 
+      new(client).retrieve
     end
+
+    attr_reader :client, :task_items
+    def initialize(client)
+      @client = client
+      @task_items = []
+    end
+    
+    def retrieve
+      @task_items = JSON.parse(client.http_client.get("/api/3/task_lists/?access_token=#{client.token}").body).map do |task_list| 
+        TaskItem.new(task_list['id'], task_list['project_id'])  
+      end
+      self
+    end
+
+    def each 
+      @task_items.each(&:block) if block_given?
+      @task_items
+    end
+
+    private 
+
+    TaskItem = Struct.new(:id, :project_id)
   end
 end
--- spec/services/redbooth/task_list_spec.rb
+++ spec/services/redbooth/task_list_spec.rb
@@ -11,10 +11,10 @@ describe Redbooth::TaskList do
   let(:refresh_token) { 'cd406c3dcbeb84cae0a1dd5230a6f11806751d8fa7cb1e006f7239540b381a7e' }
   let(:client) { Redbooth.(client_id, secret_id, access_token, refresh_token) }
 
-  describe '#call' do 
+  describe '.retrieve' do 
     it 'returns a list of tasks' do 
       VCR.use_cassette('task-lists') do 
-        expect(Redbooth::TaskList.(client).first["id"]).to eql 2581036
+        expect(Redbooth::TaskList.retrieve(client).each.first.id).to eql 2581036
       end
     end
   end
