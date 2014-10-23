c37a3dc03585c339b5b2d43852708cbc43caeb97
Rafael George <george.rafael@gmail.com>
2014-10-23 09:19:17 -0400

    Add a call for sessions controller to save token and refresh token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    @redbooth = Redbooth.(ENV['REDBOOTH_CLIENT_ID'],
+    redbooth = Redbooth.(ENV['REDBOOTH_CLIENT_ID'],
               ENV['REDBOOTH_CLIENT_SECRET'],
               access_token,
               refresh_token)
+    user = User.find_by_token_and_refresh_token(redbooth.token, redbooth.refresh_token) || User.create_with_redbooth_authorization(redbooth.token, redbooth.refresh_token)
     redirect_to root_path, notice: 'Signed in with Redbooth'
   end
 
